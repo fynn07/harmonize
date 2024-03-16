329a87737476973196aa24e426cbdf317919c2db
Fynn Borja <pewdiestuff@gmail.com>
2024-03-16 15:46:37 +0800

    Fixed: User Account Table Appends Even If Username Existing

--- backend/register.php
+++ backend/register.php
@@ -4,24 +4,16 @@ include("connect.php");
 ?>
 
 <?php
-
 if (isset($_POST['registerBtn'])) {
     //retrieve data from form and save the value to a variable
     //for tbluserprofile
     $fname = $_POST['firstname'];
     $lname = $_POST['lastname'];
     $dsplyname = $_POST['displayname'];
-    // echo "<script language='javascript'>
-    //     alert('hi nin');
-    // </script>";
-    //for tbluseraccount
+
     $email = $_POST['email'];
     $uname = $_POST['username'];
     $pword = $_POST['password'];
-
-    //save data to tbluserprofile
-    $sql1 = "Insert into tbluserprofile(registerFname, registerLname, registerDsplyname) values('" . $fname . "','" . $lname . "','" . $dsplyname . "')";
-    mysqli_query($connection, $sql1);
 
     //Check tbluseraccount if username is already existing. Save info if false. Prompt msg if true.
     $sql2 = "Select * from tbluseraccount where registerUsername='" . $uname . "'";
@@ -29,6 +21,10 @@ if (isset($_POST['registerBtn'])) {
     $row = mysqli_num_rows($result);
 
     if ($row == 0) {
+    //save data to tbluserprofile
+        $sql1 = "Insert into tbluserprofile(registerFname, registerLname, registerDsplyname) values('" . $fname . "','" . $lname . "','" . $dsplyname . "')";
+        mysqli_query($connection, $sql1);
+
         $sql = "Insert into tbluseraccount(registerUsername, registerPassword, registerEmail) values('" . $uname . "','" . $pword . "','" . $email . "')";
         mysqli_query($connection, $sql);
         echo "<script language='javascript'>
